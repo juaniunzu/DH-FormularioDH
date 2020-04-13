40a84d884372bc20430f53a9b792db84c519fb04
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-04-13 19:08:33 -0300

    envio activity layout

---
+++ app/src/main/java/com/example/formulariodh/EnvioActivity.java
@@ -0,0 +1,14 @@
+package com.example.formulariodh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnvioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_envio);
+    }
+}
